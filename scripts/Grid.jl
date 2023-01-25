a9e7ece9377ad08bbd40a6a23e7144a271a3958f
Tuan Nguyen <tuannguyen20071844@gmail.com>
2023-01-25 14:53:23 -0500

    add grid scripts and notebook to build LP operator

---
+++ scripts/Grid.jl
@@ -0,0 +1,36 @@
+function flatten(A::Array{T,2}) where {T <: Real}
+    reduce(vcat,A)
+end
+
+function flatten(A::Array{T,3}) where {T <: Real}
+    reduce(vcat,A)
+end
+
+function unflatten(A::Array{T,1},n::Int64) where {T <: Real}
+    reshape(A,(n,n))
+end
+
+function unflatten(A::Array{T,1},n::Int64,m::Int64) where {T <: Real}
+    reshape(A,(n,n,m))
+end
+
+function kernmat(kernfun::Function,l::Float64,n::Int64)
+    km = zeros(n^2,n^2)
+    
+    dA = (l/n)^2
+    pts = [0:n-1;]*l/n
+    xs = flatten(pts' .* ones(n))
+    ys = flatten(ones(n)' .* pts)
+    
+    dxs = xs .- xs'
+    dxs[dxs .>  l/2] .= dxs[dxs .>  l/2] .- l
+    dxs[dxs .< -l/2] .= dxs[dxs .< -l/2] .+ l
+    dys = ys .- ys'
+    dys[dys .>  l/2] .= dys[dys .>  l/2] .- l
+    dys[dys .< -l/2] .= dys[dys .< -l/2] .+ l
+    dls = sqrt.(dxs.^2 + dys.^2)
+    
+    km .= kernfun.(dls)
+    km .*= dA
+    return km
+end
