81e7c6d0c6fd251ccbb0ac1e4af429dc06e7cd4e
AlexHCasares <110500252+AlexHCasares@users.noreply.github.com>
2023-04-21 16:52:59 -0400

    add alex user to init and misc

--- dev_ori_sel_RF/tools/misc.py
+++ dev_ori_sel_RF/tools/misc.py
@@ -51,6 +51,8 @@ def load_external_params(filename,verbose=True):
 		file_path = os.path.join(current_dir,data_dir,filename+".yaml")
 	elif os.environ["USER"]=="thn2112":
 		file_path = os.path.join("/burg/theory/users/thn2112/",data_dir,filename+".yaml")
+	elif os.environ["USER"]=="alex":
+		file_path = os.path.join(current_dir,data_dir,filename+".yaml")
 	else:
 		raise Exception("User not found. Can't load parameter file.")
 
