3420db10f8c3f9c97c7028552bbe148dd615015c
Tuan Nguyen <tuannguyen20071844@gmail.com>
2023-08-22 11:14:02 -0400

    add linear combinations of t-phase solns

--- scripts/Grid.jl
+++ scripts/Grid.jl
@@ -1,16 +1,16 @@
-function flatten(A::Array{T,2}) where {T <: Real}
+function flatten(A::Array{T,2}) where {T <: Number}
     reduce(vcat,A)
 end
 
-function flatten(A::Array{T,3}) where {T <: Real}
+function flatten(A::Array{T,3}) where {T <: Number}
     reduce(vcat,A)
 end
 
-function unflatten(A::Array{T,1},n::Int64) where {T <: Real}
+function unflatten(A::Array{T,1},n::Int64) where {T <: Number}
     reshape(A,(n,n))
 end
 
-function unflatten(A::Array{T,1},n::Int64,m::Int64) where {T <: Real}
+function unflatten(A::Array{T,1},n::Int64,m::Int64) where {T <: Number}
     reshape(A,(n,n,m))
 end
 
@@ -31,4 +31,31 @@ function kernmat(kernfun::Function,l::Float64,n::Int64)
     km .= kernfun.(dls)
     km .*= dA
     return km
+end
+
+function kernvec(kernfun::Function,l::Float64,n::Int64,pt::Array{Int64,1}=[n÷2,n÷2])
+    km = zeros(n^2)
+    
+    pts = [0:n-1;]*l/n
+    xs = flatten(pts' .* ones(n))
+    ys = flatten(ones(n)' .* pts)
+    
+    dxs = abs.(xs .- pt[1]*l/n)
+    dxs[dxs .>  l/2] .= l .- dxs[dxs .>  l/2]
+    dys = abs.(ys .- pt[2]*l/n)
+    dys[dys .>  l/2] .= l .- dys[dys .>  l/2]
+    dls = sqrt.(dxs.^2 + dys.^2)
+    
+    km .= kernfun.(dls)
+    return km
+end
+
+function wavevec(k::Array{Float64,1},l::Float64,n::Int64,pt::Array{Int64,1}=[n÷2,n÷2])
+    pts = [0:n-1;]*l/n
+    xs = flatten((pts .- pt[1]*l/n)' .* ones(n))
+    ys = flatten(ones(n)' .* (pts .- pt[1]*l/n))
+    xs[xs .>  l/2] .-= l
+    ys[ys .>  l/2] .-= l
+    
+    return exp.(1im*(k[1]*xs+k[2]*ys))
 end
