fix normalization of heterogeneous weight matrix

dev_ori_sel_RF/connectivity.py
@@ -193,8 +193,8 @@ class Connectivity:
             if arbor is not None:
                 disc_gaussian[np.logical_not(arbor)] = 0.
                 if self.verbose: print("arbor",arbor.shape)
-            if profile in ("Gaussian_norm"):
-                norm_factor = 2*np.pi*sigma**2
+            if profile == "Gaussian_norm":
+                norm_factor = 2*np.pi*sigma**2*self.from_size[0]**2
             else:
                 norm_factor = np.sum(disc_gaussian,axis=(2,3))[:,:,None,None]
             conn_matrix = ampl * disc_gaussian / norm_factor
@@ -812,6 +812,8 @@ class Connectivity_2pop():
         Wrec[to_Ne_total:,from_Ne_total:] = -conn_II
 
         # normalise conn matrix if the maximal real eigenvalue is given
+        if max_ew is None:
+            pass
         if Wrec.shape[0]==Wrec.shape[1]:
             ew,_ = linalg.eig(Wrec,right=True)
             if self.verbose: print("orig max ew",np.nanmax(np.real(ew)),max_ew)

scripts/runjob_sim_L23_corr_ssn.py
@@ -90,7 +90,7 @@ def runjobs():
     
     H = 0.7
     eta = 1e-2
-    grecs = 1.02*np.linspace(1,1/0.95,5)
+    grecs = 1.02*np.linspace(0.95,1.05,5)
     seeds = range(1)
 
     for grec in grecs:

scripts/sim_L23_corr_relu.py
@@ -53,7 +53,7 @@ config_dict["W4to4_params"]["mean_eccentricity"] = H
 config_dict["W4to4_params"]["SD_eccentricity"] = 0.025*H
 config_dict["W4to4_params"]["SD_size"] = 0.003*H
 
-W4 = connectivity.Connectivity_2pop((N,N),(N,N),\
+conn = connectivity.Connectivity_2pop((N,N),(N,N),\
                                     (N,N), (N,N),\
                                     random_seed=seed,\
                                     Nvert=1, verbose=True)
@@ -61,10 +61,10 @@ W4 = connectivity.Connectivity_2pop((N,N),(N,N),\
 start = time.process_time()
 
 try:
-    W4to4 = np.load('./../notebooks/hetero_W_N={:d}_H={:.1f}_seed={:d}.npy'.format(N,H,seed))
+    Wrec = np.load('./../notebooks/hetero_W_N={:d}_H={:.1f}_seed={:d}.npy'.format(N,H,seed))
 except:
-    W4to4,_ = W4.create_matrix_2pop(config_dict["W4to4_params"],config_dict["W4to4_params"]["Wrec_mode"])
-    np.save('./../notebooks/hetero_W_N={:d}_H={:.1f}_seed={:d}'.format(N,H,seed),W4to4)
+    Wrec,_ = conn.create_matrix_2pop(config_dict["W4to4_params"],config_dict["W4to4_params"]["Wrec_mode"])
+    np.save('./../notebooks/hetero_W_N={:d}_H={:.1f}_seed={:d}'.format(N,H,seed),Wrec)
 
 print('Creating heterogeneous recurrent connectivity took',time.process_time() - start,'s\n')
 
@@ -112,7 +112,7 @@ rates = np.zeros_like(inps)
 start = time.process_time()
 
 for inp_idx in range(n_inp):
-    rates[inp_idx] = integrate(np.ones(2*N**2),inps[inp_idx].reshape((2,-1)),W4to4,0.25,n_int,grec)
+    rates[inp_idx] = integrate(np.ones(2*N**2),inps[inp_idx].reshape((2,-1)),Wrec,0.25,n_int,grec)
 
 res_dict['rates'] = rates
 

scripts/sim_L23_corr_ssn.py
@@ -53,7 +53,7 @@ config_dict["W4to4_params"]["mean_eccentricity"] = H
 config_dict["W4to4_params"]["SD_eccentricity"] = 0.025*H
 config_dict["W4to4_params"]["SD_size"] = 0.003*H
 
-W4 = connectivity.Connectivity_2pop((N,N),(N,N),\
+conn = connectivity.Connectivity_2pop((N,N),(N,N),\
                                     (N,N), (N,N),\
                                     random_seed=seed,\
                                     Nvert=1, verbose=True)
@@ -61,12 +61,12 @@ W4 = connectivity.Connectivity_2pop((N,N),(N,N),\
 start = time.process_time()
 
 try:
-    W4to4 = np.load('./../notebooks/hetero_W_N={:d}_H={:.1f}_seed={:d}.npy'.format(N,H,seed))
+    Wrec = np.load('./../notebooks/hetero_W_N={:d}_H={:.1f}_seed={:d}.npy'.format(N,H,seed))
 except:
-    W4to4,_ = W4.create_matrix_2pop(config_dict["W4to4_params"],config_dict["W4to4_params"]["Wrec_mode"])
-    np.save('./../notebooks/hetero_W_N={:d}_H={:.1f}_seed={:d}'.format(N,H,seed),W4to4)
+    Wrec,_ = conn.create_matrix_2pop(config_dict["W4to4_params"],config_dict["W4to4_params"]["Wrec_mode"])
+    np.save('./../notebooks/hetero_W_N={:d}_H={:.1f}_seed={:d}'.format(N,H,seed),Wrec)
 
-W4to4 = W4to4.reshape((2,N**2,2,N**2)).transpose((0,2,1,3))
+Wrec = Wrec.reshape((2,N**2,2,N**2)).transpose((0,2,1,3))
 
 print('Creating heterogeneous recurrent connectivity took',time.process_time() - start,'s\n')
 
@@ -76,7 +76,7 @@ kI = 0.05
 nE = 2
 nI = 3
 
-a = W4to4.sum(-1).mean(-1)
+a = Wrec.sum(-1).mean(-1)
 k = np.array([kE,kI])
 n = np.array([nE,nI])
 
@@ -136,7 +136,7 @@ rates = np.zeros_like(inps)
 start = time.process_time()
 
 for inp_idx in range(n_inp):
-    rates[inp_idx] = integrate(np.ones((2,N**2)),inps[inp_idx].reshape((2,-1)),W4to4,grec*gam,k,n,0.25,n_int)
+    rates[inp_idx] = integrate(np.ones((2,N**2)),inps[inp_idx].reshape((2,-1)),Wrec,grec*gam,k,n,0.25,n_int)
 
 res_dict['rates'] = rates
 

scripts/sim_L23_sel_ssn.py
@@ -24,6 +24,7 @@ parser.add_argument('--seed', '-s', help='seed',type=int, default=0)
 parser.add_argument('--ksel', '-k', help='selectivity shape',type=float, default=0.1)
 parser.add_argument('--lker', '-l', help='arbor length from L4 to L2/3',type=float, default=0.01)
 parser.add_argument('--grec', '-g', help='L2/3 recurrent weight strength',type=float, default=1.02)
+parser.add_argument('--gff', '-f', help='L2/3 input strength',type=float, default=1.02)
 args = vars(parser.parse_args())
 n_inp = int(args['n_inp'])
 n_int= int(args['n_int'])
@@ -31,6 +32,7 @@ seed = int(args['seed'])
 ksel = args['ksel']
 lker = args['lker']
 grec = args['grec']
+gff = args['gff']
 
 lker2 = lker**2
 
@@ -81,7 +83,7 @@ a = W4to4.sum(-1).mean(-1)
 k = np.array([kE,kI])
 n = np.array([nE,nI])
 
-I = 1*np.array([1,1])
+I = gff*np.array([1,1])
 
 V = np.linalg.inv(np.eye(2)-grec * a/n[None,:])@I
 R = k*(V**n)
@@ -205,7 +207,7 @@ def integrate(y0,inp,Wrec,gamma_rec,k,n,dt,Nt):
         out = dynamics_system(y,inp,Wrec,gamma_rec,1.0,k,n,1.0)
         dy = out
         y = y + dt*dy
-    return np.array([y[0].reshape((N,N)),y[1].reshape((N,N))])
+    return np.array([y[0].reshape((N4,N4)),y[1].reshape((N4,N4))])
 
 # Integrate to get firing rates
 rates = np.zeros_like(inps)
@@ -213,7 +215,7 @@ rates = np.zeros_like(inps)
 start = time.process_time()
 
 for inp_idx in range(n_inp):
-    rates[inp_idx] = integrate(np.ones((2,N**2)),inps[inp_idx].reshape((2,-1)),W4to4,grec*gam,k,n,0.25,n_int)
+    rates[inp_idx] = integrate(np.ones((2,N4**2)),inps[inp_idx].reshape((2,-1)),W4to4,grec*gam,k,n,0.25,n_int)
     
 print('Simulating rate dynamics took',time.process_time() - start,'s\n')
 