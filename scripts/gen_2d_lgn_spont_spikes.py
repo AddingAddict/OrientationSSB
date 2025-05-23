cf0c155eebba0473f349ad6b851172457724ece1
Tuan Nguyen <tuannguyen20071844@gmail.com>
2025-05-23 11:07:23 -0400

    add scripts to generate spontaneous lgn spike counts

---
+++ scripts/gen_2d_lgn_spont_spikes.py
@@ -0,0 +1,114 @@
+import sys
+import os
+try:
+    import pickle5 as pickle
+except:
+    import pickle
+sys.path.insert(0, './..')
+
+import argparse
+import time
+
+import numpy as np
+from scipy.stats import poisson,zscore
+
+import burst_func as bf
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--n_wave', '-nw', help='number of geniculate waves',type=int, default=20)
+parser.add_argument('--n_grid', '-ng', help='number of points per grid edge',type=int, default=20)
+parser.add_argument('--seed', '-s', help='seed',type=int, default=0)
+args = vars(parser.parse_args())
+n_wave = int(args['n_wave'])
+n_grid = int(args['n_grid'])
+seed = int(args['seed'])
+
+n_bar = int(np.round(2*n_grid))
+bar_len = 0.99/np.sqrt(2)
+res = 1.001*bar_len/n_bar/np.sqrt(2)
+
+rm = 16 # Hz
+rb = 0 # Hz
+dt = 0.1 # s
+ibi = 14.4 # s
+dur = 14.4 # s
+
+rs = 0.00
+ro = -0.35
+
+corr_len = 0.1
+
+rng = np.random.default_rng(seed)
+
+start = time.process_time()
+
+ss,ts = np.meshgrid(np.linspace(0.5/n_bar,1-0.5/n_bar,n_bar),np.linspace(0.5/n_bar,1-0.5/n_bar,n_bar))
+ss,ts = ss.flatten(),ts.flatten()
+xs,ys = np.meshgrid(np.linspace(0.5/n_grid,1-0.5/n_grid,n_grid),np.linspace(0.5/n_grid,1-0.5/n_grid,n_grid))
+xs,ys = xs.flatten(),ys.flatten()
+
+dxs = np.abs(xs[:,None]-xs[None,:])
+dxs = np.fmin(dxs,1-dxs)
+dys = np.abs(ys[:,None]-ys[None,:])
+dys = np.fmin(dys,1-dys)
+dists = np.sqrt(dxs**2 + dys**2)
+dist_corrs = np.exp(-0.5*(dists/corr_len)**2)
+
+oris = rng.uniform(0,2*np.pi,n_wave)
+start_poss = rng.uniform(0,1,(n_wave,2))
+
+spike_ls = np.zeros((int(np.round(ibi*n_wave/dt)),2*n_grid**2))
+spike_ls += rb * dt
+
+width = 0.25
+
+for widx in range(n_wave):
+    for tidx in range(int(np.round(dur/dt))):
+        time_idx = int(np.round(ibi/dt))*widx + tidx
+        edge_fact = 0.5 if tidx==0 or tidx==int(np.round(dur/dt))-1 else 1
+        vel = bar_len * np.array([np.cos(oris[widx]),np.sin(oris[widx])])
+        bar_to_box = bf.gen_mov_bar(start_poss[widx]+(tidx/int(dur/dt))*vel,
+            oris[widx],bar_len,bar_len)
+        bar_pos = bar_to_box(ss,ts)
+        pass_times = np.abs(bf.bar_pass_time(bar_pos,np.array([xs,ys]).T,ts,res) - 0.5)
+        for cidx in range(n_grid**2):
+            if np.isnan(pass_times[cidx]):
+                continue
+            spike_ls[time_idx,cidx] += edge_fact*(rm-rb)*\
+                np.exp(-(np.abs(pass_times[cidx])/(width))**2) * dt
+            spike_ls[time_idx,n_grid**2+cidx] += edge_fact*(rm-rb)*\
+                np.exp(-(np.abs(pass_times[cidx])/(width))**2) * dt
+spike_ls = np.fmax(1*dt,spike_ls)
+
+print('Generating spike statistics took',time.process_time() - start,'s\n')
+
+start = time.process_time()
+
+spikes = np.zeros((int(np.round(ibi*n_wave/dt)),2*n_grid**2),np.ushort)
+
+for idx,l in enumerate(spike_ls):
+    r = np.block([[rs*dist_corrs,ro*dist_corrs],
+                  [ro*dist_corrs,rs*dist_corrs]])
+    spikes[idx] = bf.gen_corr_pois_vars(l,r,rng)[:,0]
+    
+print('Generating spike counts took',time.process_time() - start,'s\n')
+
+# Define where to save results
+res_dir = './../results/'
+if not os.path.exists(res_dir):
+    os.makedirs(res_dir)
+
+res_dir = res_dir + '2d_lgn_spont_spikes_nw={:d}_ng={:d}/'.format(n_wave,n_grid)
+if not os.path.exists(res_dir):
+    os.makedirs(res_dir)
+
+res_file = res_dir + 'seed={:d}.pkl'.format(seed)
+
+res_dict = {}
+
+# res_dict['spike_ls'] = spike_ls
+# res_dict['spike_rs'] = spike_rs
+res_dict['spikes'] = spikes
+
+with open(res_file, 'wb') as handle:
+    pickle.dump(res_dict,handle)
---
+++ scripts/runjob_gen_2d_lgn_spont_spikes.py
@@ -0,0 +1,133 @@
+import os
+import argparse
+import numpy as np
+from subprocess import Popen
+import time
+from sys import platform
+import uuid
+import random
+
+from importlib import reload
+
+def runjobs():
+
+
+    """
+        Function to be run in a Sun Grid Engine queuing system. For testing the output, run it like
+        python runjobs.py --test 1
+        
+    """
+    
+    #--------------------------------------------------------------------------
+    # Test commands option
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--test", "-t", type=int, default=0)
+    parser.add_argument("--cluster_", help=" String", default='burg')
+    parser.add_argument('--n_wave', '-nw', help='number of geniculate waves',type=int, default=20)
+    parser.add_argument('--n_grid', '-ng', help='number of points per grid edge',type=int, default=20)
+    parser.add_argument('--gb', '-g', help='number of gbs per cpu',type=int, default=100)
+    
+    args2 = parser.parse_args()
+    args = vars(args2)
+    
+    cluster = str(args["cluster_"])
+    n_wave = int(args['n_wave'])
+    n_grid = int(args['n_grid'])
+    gb = int(args['gb'])
+
+    
+    if (args2.test):
+        print ("testing commands")
+    
+    #--------------------------------------------------------------------------
+    # Which cluster to use
+
+    
+    if platform=='darwin':
+        cluster='local'
+    
+    currwd = os.getcwd()
+
+    #--------------------------------------------------------------------------
+    # Ofiles folder
+
+    user = os.environ["USER"]
+        
+    if cluster=='haba':
+        path_2_package="/rigel/theory/users/"+user+"/OrientationSSB/scripts"
+        ofilesdir = path_2_package + "/Ofiles/"
+        resultsdir = path_2_package + "/results/"
+
+    if cluster=='moto':
+        path_2_package="/moto/theory/users/"+user+"/OrientationSSB/scripts"
+        ofilesdir = path_2_package + "/Ofiles/"
+        resultsdir = path_2_package + "/results/"
+
+    if cluster=='burg':
+        path_2_package="/burg/theory/users/"+user+"/OrientationSSB/scripts"
+        ofilesdir = path_2_package + "/Ofiles/"
+        resultsdir = path_2_package + "/results/"
+        
+    elif cluster=='axon':
+        path_2_package="/home/"+user+"/OrientationSSB/scripts"
+        ofilesdir = path_2_package + "/Ofiles/"
+        resultsdir = path_2_package + "/results/"
+        
+    elif cluster=='local':
+        path_2_package="/Users/tuannguyen/OrientationSSB/scripts"
+        ofilesdir = path_2_package+"/Ofiles/"
+        resultsdir = path_2_package + "/results/"
+
+
+    if not os.path.exists(ofilesdir):
+        os.makedirs(ofilesdir)
+
+    if not os.path.exists(resultsdir):
+        os.makedirs(resultsdir)
+
+    time.sleep(0.2)
+    
+    seeds = range(10)
+
+    for seed in seeds:
+        #--------------------------------------------------------------------------
+        # Make SBTACH
+        inpath = currwd + "/gen_2d_lgn_spont_spikes.py"
+        c1 = "{:s} -s {:d} -nw {:d} -ng {:d}".format(
+            inpath,seed,n_wave,n_grid)
+        
+        jobname="{:s}_nw={:d}_ng={:d}_seed={:d}".format(
+            'gen_2d_lgn_spont_spikes',n_wave,n_grid,seed)
+        
+        if not args2.test:
+            jobnameDir=os.path.join(ofilesdir, jobname)
+            text_file=open(jobnameDir, "w");
+            os. system("chmod u+x "+ jobnameDir)
+            text_file.write("#!/bin/sh \n")
+            if cluster=='haba' or cluster=='moto' or cluster=='burg':
+                text_file.write("#SBATCH --account=theory \n")
+            text_file.write("#SBATCH --job-name="+jobname+ "\n")
+            text_file.write("#SBATCH -t 0-11:59  \n")
+            text_file.write("#SBATCH --mem-per-cpu={:d}gb \n".format(gb))
+            # text_file.write("#SBATCH --gres=gpu\n")
+            text_file.write("#SBATCH -c 1 \n")
+            text_file.write("#SBATCH -o "+ ofilesdir + "/%x.%j.o # STDOUT \n")
+            text_file.write("#SBATCH -e "+ ofilesdir +"/%x.%j.e # STDERR \n")
+            text_file.write("python  -W ignore " + c1+" \n")
+            text_file.write("echo $PATH  \n")
+            text_file.write("exit 0  \n")
+            text_file.close()
+
+            if cluster=='axon':
+                os.system("sbatch -p burst " +jobnameDir);
+            else:
+                os.system("sbatch " +jobnameDir);
+        else:
+            print (c1)
+
+
+
+if __name__ == "__main__":
+    runjobs()
+
+
