3493a7cb452e1ef3c1d0a089d7fd9a88d2bb8e98
AddingAddict <tuannguyen20071844@gmail.com>
2025-10-21 11:38:01 -0400

    new dev model results

--- scripts/sim_L4_act_L23_sel.py
+++ scripts/sim_L4_act_L23_sel.py
@@ -120,7 +120,7 @@ def integrate_sheet(xea0,xen0,xeg0,xia0,xin0,xig0,inp,Jee,Jei,Jie,Jii,kerne,kern
     xin = xin0.copy()
     xig = xig0.copy()
     
-    rng = np.random.default_rng(0)
+    rng = np.random.default_rng(seed)
     
     noise = rng.gamma(shape=1/het_lev**2,scale=het_lev**2,size=(N**2,N**2))
     Wee = Jee*kerne.reshape(N**2,N**2)*noise
@@ -312,7 +312,7 @@ if saveweights:
     
     het_lev = params[8]
     
-    rng = np.random.default_rng(0)
+    rng = np.random.default_rng(seed)
     
     noise = rng.gamma(shape=1/het_lev**2,scale=het_lev**2,size=(N**2,N**2))
     Wee = Jee*kerne.reshape(N**2,N**2)*noise
